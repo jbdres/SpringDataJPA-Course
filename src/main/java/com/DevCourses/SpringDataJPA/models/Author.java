8cc3558123be598c53679e8d8f771b8e3b931ed8
jbdres <julian.betov@gmail.com>
2025-03-18 20:40:00 -0500

    Many-to-many relationship between Author and Course entities

--- src/main/java/com/DevCourses/SpringDataJPA/models/Author.java
+++ src/main/java/com/DevCourses/SpringDataJPA/models/Author.java
@@ -1,15 +1,13 @@
 package com.DevCourses.SpringDataJPA.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -30,5 +28,21 @@ public class Author {
     private LocalDateTime createdAt;
     @Column(insertable = false)
     private LocalDateTime lastModified;
+
+    // Inverse of the relationship
+    /* - @ManyToMany(mappedBy = "value")
+     *   -> value is the name of the attribute in the entity that owns the relationship
+     *      that refers this class.
+     * */
+    @ManyToMany(mappedBy = "authors")
+    private List<Course> courses;
+
+    /* Explanation:
+     * - @ManyToMany(mappedBy = "authors"): This annotation again signifies a many-to-many relationship.
+     *     -> mappedBy = "authors": This is crucial for bidirectional relationships. It tells JPA that the courses list in the Author entity is
+     *        mapped by the authors list in the Course entity. Essentially, it indicates that the Course entity is the owning side of the
+     *        relationship. This is important because only the owning side is responsible for updating the join table.
+     * - private List<Course> courses;: This declares a list of Course objects within the Author entity. This list represents the courses authored by the author.
+     * */
 
 }
