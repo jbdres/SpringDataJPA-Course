5d28de742bfc90acad3846bc7002d095cedce0c9
jbdres <julian.betov@gmail.com>
2025-03-19 18:31:30 -0500

    One-to-many relationship between Section and Lecture entities

--- src/main/java/com/DevCourses/SpringDataJPA/models/Lecture.java
+++ src/main/java/com/DevCourses/SpringDataJPA/models/Lecture.java
@@ -1,8 +1,6 @@
 package com.DevCourses.SpringDataJPA.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,5 +17,9 @@ public class Lecture {
     @GeneratedValue
     private Integer id;
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private Section section;
 
 }
